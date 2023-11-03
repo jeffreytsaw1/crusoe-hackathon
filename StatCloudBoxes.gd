91838f543f9b2e140fddf290593b08369e984e2f
Anna Landler <alandler@crusoeenergy.com>
2023-11-02 17:43:08 -0700

    money labels updated

--- StatCloudBoxes.gd
+++ StatCloudBoxes.gd
@@ -1,6 +1,12 @@
 extends RichTextLabel
 
 func _ready():
+	updateText()
+
+func _process(_delta):
+	updateText()
+	
+func updateText():
 	# BBCode string with an image and text
 	var bbcode_text = "[img]res://cloud_up.png[/img] [color=#ffbb22] Hashy " + str(Global.num_swe) +" [/color]"
 	text = bbcode_text
--- moneyLabel.gd
+++ moneyLabel.gd
@@ -1,6 +1,11 @@
 extends RichTextLabel
 
 func _ready():
-	# BBCode string with an image and text
+	update()
+
+func _process(_delta):
+	update()
+
+func update():
 	var bbcode_text = "[img]res://cloud_up.png[/img] [color=#ffbb22] Money " + str(Global.money) +" [/color]"
 	text = bbcode_text
